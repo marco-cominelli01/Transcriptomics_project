35975602bfde18a7ea7c0628f68447153da5ccd5
marco-cominelli01 <cominellimarco8@gmail.com>
2024-05-19 20:53:48 +0200

    Update Script_transcriptomics.R

--- Script_transcriptomics.R
+++ Script_transcriptomics.R
@@ -1,1 +1,21 @@
-# Ciao
+### For bulk RNA-Seq
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager")
+}
+
+BiocManager::install("recount3")
+
+BiocManager::install(c(
+  "recount", "GenomicRanges", "limma", "edgeR", "DESeq2",
+  "regionReport", "clusterProfiler", "org.Hs.eg.db", "gplots",
+  "derfinder", "GenomicState", "bumphunter", "derfinderPlot", "sessioninfo"
+))
+
+### For single cell RNA_Seq
+install.packages("tidyverse")
+install.packages('Seurat')
+library(Seurat)  # In case of Warning message, enter 'y'
+
+install.packages("devtools")
+devtools::install_github("thomasp85/patchwork")
+
