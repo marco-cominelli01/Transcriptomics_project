24513f923eabb3d0d2d6a10625ec8174b78f2e79
marco-cominelli01 <cominellimarco8@gmail.com>
2024-05-21 18:31:00 +0200

    Update Lecture_21st_May_recount_vignettas_and_DE_analysis.R

--- Lecture_21st_May_recount_vignettas_and_DE_analysis.R
+++ Lecture_21st_May_recount_vignettas_and_DE_analysis.R
@@ -3,45 +3,41 @@ library(recount)
 library(edgeR)
 
 rse_brain <- readRDS("rse_brain.RDS")
-rse_colon <- readRDS("rse_colon.RDS")
-rse_muscle <- readRDS("rse_muscle.RDS")
 
-# Those are brain samples: 2931 columns (= different individuals) and 54042 rows (= genes)
-# rowData: I have 25 additional annotations;
-# colData: 198 additional piece of information for each sample.
+# Those are brain samples: 2931 columns (= different individuals) and 54042 rows (= genes).
+# rowData: I have 25 additional annotations.
+# colData: I have 198 additional pieces of information for each sample.
 rse_brain
 
-# The counts are in "coverage" format
+# The counts are in "coverage" format.
 assays(rse_brain)$counts <- transform_counts(rse_brain)
 
-# I can see wheter or not I have enough information to convert the counts I find in table
-# to obtain CPM (I need library size and gene length informations)
+# I can see whether or not I have enough information to convert the counts I find 
+# in the table to obtain the CPM (I need the library size and the gene length)
 
+# Here I see all the additional informations for each sample: sex, age,
+# the one starting with 'recount' contain the results of their processes
+# (of the people working for this package), % of reads mapping on chrX and chrY,
+# 'qc.star' contain all the parameters and results obtained by the output of STAR
+# used for the mapping
 names(colData(rse_brain))
-# Tutte le infor aggiuntive per ogni sample; tutto ciò che inizia per gtex
-# è related to the sample; ho il sesso e età del donor; 
-# recount ho tutti i risultati coming from their processes;
-# 77 e 78 %di reads mapping on chromosome x and Y
-# qc.star --> all the parameters and results obtained by the output using STAR for mapping
 
-
+# RIN value of each sample (number that goes from 0 to 10)
 table(colData(rse_brain)$gtex.smrin)
-# Il RIN value di ogni sample (numero da 0 a 10)
 
+# It tells me the specific portion of the brain each of the sample comes from.
+# So here we have different individuals and different portions, so very high variability.
 table(colData(rse_brain)$gtex.smtsd)
-#  mi dice la specifica portion del brain each of the sample came from.
-# sono diversi individui e diverse parti del cervello, quindi la variability è mlto alta
 
-table(colData(rse_brain)$gtex.sex) # Biologicaylly dovrebbe essere con o senza chrY nei dati
+table(colData(rse_brain)$gtex.sex) # Look at the presence/absence of chrY in data 
 
-table(colData(rse_brain)$gtex.age)
-# Devo mettere range per privacy issue
+table(colData(rse_brain)$gtex.age) # They are written in range because of privacy issues.
 
+# Number of reads before the mapping (paired-end reads)
 head(colData(rse_brain)$"recount_qc.star.number_of_input_reads_both")
-# Number of reads before the mapping (paired end reads)
 
+# % of uniquely mapped reads --> the ones employed for computing the read counts
 head(colData(rse_brain)$'recount_qc.star.uniquely_mapped_reads_%_both')
-# and the percentage of uniquely mapped reads - the ones employed for computing the read counts:
 
 inputreads <- colData(rse_brain)$"recount_qc.star.number_of_input_reads_both"
 boxplot(inputreads)
@@ -52,10 +48,9 @@ minlib
 maxlib
 
 mapped <- colData(rse_brain)$"recount_qc.star.uniquely_mapped_reads_%_both"
-boxplot(mapped)
-# distribuzione di % mapped reads on the genome; il boxplot è way over 80% 
-# ma ci sono molti outliers, alcuni sample hanno % di mapping reads bassissime
-# il minimo è 6.6%
+boxplot(mapped) # Distribution of % of mapped reads on the genome: the boxplot is
+                # way over 80% but there are a lot of outliers which have a very
+                # low mapping (the lowest is 6.6%)
 min(mapped)
 
 min_map <- which.min(mapped)
@@ -63,50 +58,51 @@ colData(rse_brain)[min_map,]
 
 
 rinplot <- colData(rse_brain)$gtex.smrin
-boxplot(rinplot)
-# Median is on 7, few outliers very good, ma some at 5 --> controllo RIN prima di seuencing
-# quindi loro sono stati molto permissivi perché il più basso è 5
+boxplot(rinplot) # Median is good (7) and there are few outliers, but some are at 5
+                 # --> the RIN is checked before the sequencing, so the authors were
+# very permissive in sequencing also the samples with RIN = 5
 
-#Questi erano total RNA seq: quindi guardiamo esimtated % of rRNA in my samples
-head(colData(rse_brain)$gtex.smrrnart) # il consiglio è rimane sotto il 10% di rRNA
+# These were total RNA-Seq: let's look at the estimated % of rRNA in my samples
+head(colData(rse_brain)$gtex.smrrnart) # Under 10% is good
 boxplot(colData(rse_brain)$gtex.smrrnart)
 
 head(colData(rse_brain)$"recount_qc.aligned_reads%.chrm") 
-boxplot(colData(rse_brain)$"recount_qc.aligned_reads%.chrm") # samples in cui 
-# 70% of reads were due only to mtDNA
-
-
+boxplot(colData(rse_brain)$"recount_qc.aligned_reads%.chrm") # There are samples with
+                                                             # 70% of mtDNA
 
 plot(colData(rse_brain)$gtex.smrrnart, colData(rse_brain)$"recount_qc.aligned_reads%.chrm")
-# % of rRNA va % of mtDNA, ogni punto è un sample
-# --> perfect correlation: più ribosomal ho, più mitocondrial
+# % of rRNA va % of mtDNA, each point is a sample.
+# --> perfect correlation: more ribosomal I have, more mitocondrial I have
 # --> why? 
 
-names(rowData(rse_brain)) # Info for all genes; ho name, gene_name, gene_id, gene_id, gene_synonym...
-# they put to each gene annotation to what the gene is:
-# produce mRNA, o rRNA, o tRNA, o 'Gene' whch means pseudo-gene, 
-# C_Region, D_segment, V_ssegment --> loci where there is individual variability (immunoglobulin)
-# "Gene" is indeed a pseudogene --> genomic regions that look like a gene
-# On the human genome there are some regions that look like protein coding regions but without
-# any split in exons and introns --> even if they look like protein coding genes,
-# at time they didnt find trace of gene activity. 2 sources: mature RNA retrotranscribed 
-# and insert back into the genome; or gene duplication e una delle due copie fu inattivata.
-# dato che ora sequenzimao molto, le persone hanno visto che gli pseudo gene si comportano da 
-# geni, a low levels they find evidence of transcruption --> quindi invcee d iannotarli 
-# come novel genes, they kept them as a separate category.
+names(rowData(rse_brain)) # All info we have for each row (so for each gene): name,
+                          # gene_name, gene_id...
 
-table(rowData(rse_brain)$gbkey)
+table(rowData(rse_brain)$gbkey) # This tells if the gene produces mRNA, it's a
+                                # pseudogene (called "Gene", which is weird), 
+                                # rRNA, tRNA...
+# We also see C_region, D_segment and so on --> those are the loci where there 
+# is the most individual variability (immunoglobulin)
+# "Gene" means that it's a pseudogene: pseudogenes are genomic regions that 
+# look like a protein coding gene but without any split in exons and introns 
+# --> even if they look like protein coding genes, at the time they didn't find 
+# any trace of gene activity. 
+# Two sources of pseudogenes: 
+# 1) Mature RNA retrotranscribedand insert back into the genome
+# 2) Gene duplication and one of the two copy was inactivated.
+# Now we sequence a lot (and very in depth) so we start to see that also pseudogenes
+# are transcribed (at low levels) --> instead of annotate them as novel genes, 
+# they kept them as a separate category.
 
-rowRanges(rse_brain)
-# bp_length is the gene lenght
+rowRanges(rse_brain) # bp_length is the gene lenght
 
-table(rowRanges(rse_brain)@seqnames)
-# Ho i chanonical chromosomes; sotto ho tutti glialternative chromosomes e via dicendo
-# quindi ho mapping su anche i non-chanonical chromosomes
+table(rowRanges(rse_brain)@seqnames) # The mapping was done on canonical 
+                                     # chromosomes, but also on the non-canonical 
+                                     # chromosomes
 
+# Several checks on the genes mapping on chrM
 mito <- rse_brain[rowRanges(rse_brain)@seqnames == 'chrM']
-dim(mito) # 
-
+dim(mito)
 mito
 rowRanges(mito)
 
